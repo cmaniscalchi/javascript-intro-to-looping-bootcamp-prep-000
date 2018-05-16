62dfed6804e37811348b2da3c3ddf3f23354696f
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-16 03:57:52 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++)
+  if (i === 0) {
+    array.push(`I am ${i} strange loop.`);
+}
+  else if (i > 0) {
+    array.push(`I am ${i} strange loops.`);
+}
+return array;
+}
+
+function whileLoop(n) {
+  let countdown = n;
+  while (countdown > 0) {
+  console.log(--countdown);
+  }
+}
