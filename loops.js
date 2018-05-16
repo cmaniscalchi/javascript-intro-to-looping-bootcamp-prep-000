e7766663dae5dcf0ca21e7c6c733436c0625c890
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-16 04:05:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,8 +12,7 @@ return array;
 function whileLoop(n) {
   let countdown = n;
   while (countdown > 0) {
-    console.log(--countdown)
-    
+    console.log(--countdown);
   }
-  return "done"
+  return "done";
 }
